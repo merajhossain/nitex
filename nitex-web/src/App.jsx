0b00da2aaaefb89af02bd27736841246067c8d57
merajhossain <merajhossain29@gmail.com>
2020-07-28 17:04:04 +0600

    confilct
    font awesome conflict solve

--- nitex-web/src/App.jsx
+++ nitex-web/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/fontawesome.scss';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import AppRoute from './components/AppRoute';
 import './styles/styles.scss';
